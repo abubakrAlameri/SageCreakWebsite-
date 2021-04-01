0b3c9ff885f26eb7740fce61ec25fec85fd2dbca
AbubakrAlameri <62396017+abubakrAlameri@users.noreply.github.com>
2021-04-01 23:20:25 +0300

    after adding calendar

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+      fetch("events.json")
+                .then(response => response.json())
+                .then(json => {
+
+                   
+                            var calendarEl = document.getElementById('calendar');
+                            var calendar = new FullCalendar.Calendar(calendarEl, {
+                                initialView: 'dayGridMonth',
+                                events: json
+
+                            });
+                            calendar.render();
+                    
+                });
+
 function show(ques){
 console.log( document.querySelector('.'+ques+'+.ans'))
     document.querySelector('.'+ques+'+.ans').classList.toggle('show');
    
 }
+
+
+  
+                 
